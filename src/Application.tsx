d199b9841db8203f9ba31c8196e3e1aa9f19bfd4
tomzacchia <tomzacchia@live.com>
2022-04-06 21:33:07 -0400

    NOTE: Optional payload in reducer action

---
+++ src/Application.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+type CounterState = {
+  value: number;
+};
+
+const initialState: CounterState = { value: 0 };
+
+type CounterAction = {
+  type: "INCREMENT" | "DECREMEMT" | "RESET";
+  payload?: number;
+};
+
+function reducer(state: CounterState, action: CounterAction) {
+  if (action.type === "INCREMENT") {
+    return { value: state.value + 1 };
+  }
+
+  if (action.type === "DECREMEMT") {
+    return { value: state.value - 1 };
+  }
+
+  if (action.type === "RESET") {
+    // since payload is optional it is possible for action.payload === undefined
+    // as such we need to provide a default value since value: number
+    return { value: action.payload || 0 };
+  }
+
+  return { ...state };
+}
+
+const Counter = () => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  return (
+    <main className="Counter">
+      <h1>Days Since Last Incident</h1>
+      <p className="count">{state.value}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+        <button onClick={() => dispatch({ type: "DECREMEMT" })}>
+          Decrement
+        </button>
+      </section>
+    </main>
+  );
+};
+
+const Application = () => <Counter />;
+
+export default Application;
